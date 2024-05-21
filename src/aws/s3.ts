38ef5813851fdc17918c22cb8b1f1424d7fdaa35
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-05-21 13:18:18 -0300

    checkpoint

--- src/aws/s3.ts
+++ src/aws/s3.ts
@@ -3,11 +3,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-aws.config.update({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-  region: process.env.REGION
-})
 
 export const s3 = new aws.S3();
 
